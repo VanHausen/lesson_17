e0abca3a69cb43e8543d762996127a2787090be4
Evgeniy <68319098+VanHausen@users.noreply.github.com>
2022-09-07 14:37:55 +0300

    Add files via upload

---
+++ part2/post/main.py
@@ -0,0 +1,61 @@
+# У вас настроенный фласк и список с данными.
+#
+# Вам необходимо:
+#
+# 1. Создать переменную app c экземпляром
+#    класса App из библиотеки flask_restx
+#
+# 2. Создать неймспейc note_ns с адресом `notes`
+#
+# 3. Создать Сlass based view который позволяет
+#    с помощью POST-запроса по адресу `/notes/`
+#    добавить в список соответствующий объект
+#
+# Пример POST-запроса:
+#
+# {
+#  "id": 4,
+#  "text": "Текст заметки идет здесь",
+#  "author": "Кто ты?"
+# }
+
+from flask import Flask, request
+from pprint import pprint
+
+from flask_restx import Api, Resource
+
+from part2.post.solution import INSTANCE
+
+app = Flask(__name__)
+app. config['RESTX_JSON'] = {'ensure_ascii': False, 'indent': 2}
+
+api = Api(app)
+note_ns = api.namespace('notes')
+
+notes = [
+    {
+        "id": 1,
+        "text": "This is my note!",
+        "author": "me",
+    },
+    {
+        "id": 2,
+        "text": "This is also my note!",
+        "author": "me",
+    }
+]
+
+@note_ns.route('/')
+class NotesView(Resource):
+    def post(self):
+        req_json = request.json
+        notes.append(req_json)
+        return "", 201
+                                                
+     
+                                                # Не удаляйте этот код, он нужен для
+if __name__ == '__main__':                      # имитации post-запроса и вывода
+    client = app.test_client()                  # результата в терминал
+    response = client.post('/notes/', json=INSTANCE)  # TODO для самопроверки вы можете добавить
+    pprint(notes, indent=2)                     # свой json в соответствующий аргумент
+                                                # функции post
